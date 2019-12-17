7ecb66ac6fc4ef5a20ca623e3663ed6bc42c6e29
Nicolas Garnier <nicolasgarnier22@hotmail.com>
2019-12-17 09:49:44 +0100

    dev

--- monapp/src/App/App.js
+++ monapp/src/App/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 // import logo from '../logo.svg';
 import './App.css';
 import Navbar from './component.Navbar/component.Navbar'
-import User from './component.User/component.User.view'
-import Users from './component.User/component.Users';
+// import User from './component.User/component.User.view'
+// import Users from './component.User/component.Users';
+
+import TchatConsole from './component.TchatConsole/component.TchatConsole';
+import TchatDisplay from './component.tchatDisplay/component.tchatDisplay';
+
 
 function App() {
   let _users=JSON.parse('[{"id":1,"nom":"GARNIER","prenom":"Nicolas","serviceId":"1"},{"id":2,"nom":"Test","prenom":"Robert","serviceId":"1"}]'
    );
   return (
     <div className="App">
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar>
       <Users users={_users}></Users>
-      <User unUser={{nom:"Bon",prenom:"Jean",serviceId:0}}></User>    
+      <User unUser={{nom:"Bon",prenom:"Jean",serviceId:0}}></User>     */}
+     {/* <TchatDisplay></TchatDisplay> */}
+      <TchatConsole></TchatConsole>
+      <TchatDisplay></TchatDisplay>
+     
             
     </div>
   );
